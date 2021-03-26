0ac10d0840a60a59cd029e61a185b1b41422a0f0
Steve Cadden <s.p.cadden@gmail.com>
2021-03-26 16:16:13 +0000

    CORS added

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -16,46 +16,60 @@ namespace Server
 {
     public class Startup
     {
+        readonly string frontEnd = "_frontEnd";
         public Startup(IConfiguration configuration)
+
         {
             Configuration = configuration;
         }
+
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+      {
+          options.AddPolicy(name: frontEnd,
+                            builder =>
+                            {
+                                builder.WithOrigins("http://localhost:3000")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .AllowCredentials();
+                            });
+      
 
-
-            services.AddTransient<IRepository<CountryObj>, CountryRepository>();
+        services.AddTransient<IRepository<CountryObj>, CountryRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Server", Version = "v1" });
             });
-        }
+        });
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Server v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
     }
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Server v1"));
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.UseCors(frontEnd);
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
 }
