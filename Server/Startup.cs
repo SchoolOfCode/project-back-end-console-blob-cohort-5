618a8eb690b6406b83c07bb626e966bcf4feb431
Char J <CharJ2889@gmail.com>
2021-03-29 13:38:00 +0100

    startup file changed

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -11,61 +11,52 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Server
 {
     public class Startup
     {
-        readonly string frontEnd = "_frontEnd";
+        // readonly string frontEnd = "_frontEnd";
         public Startup(IConfiguration configuration)
-
         {
             Configuration = configuration;
         }
-
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IRepository<CountryObj>, CountryRepository>();
+            services.AddControllers();
             services.AddCors(options =>
       {
-          options.AddPolicy(name: frontEnd,
+          options.AddDefaultPolicy(
                             builder =>
                             {
-                                builder.WithOrigins("http://localhost:3000")
+                                builder.AllowAnyOrigin()
                                 .AllowAnyHeader()
-                                .AllowAnyMethod()
-                                .AllowCredentials();
+                                .AllowAnyMethod();
                             });
-      
-
         services.AddTransient<IRepository<CountryObj>, CountryRepository>();
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Server", Version = "v1" });
-            });
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Server", Version = "v1" });
+            // });
         });
-
     }
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Server v1"));
-        }
-
+        // if (env.IsDevelopment())
+        // {
+        //     app.UseDeveloperExceptionPage();
+        //     app.UseSwagger();
+        //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Server v1"));
+        // }
         app.UseHttpsRedirection();
-
         app.UseRouting();
-        app.UseCors(frontEnd);
+        app.UseCors();
         app.UseAuthorization();
-
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
