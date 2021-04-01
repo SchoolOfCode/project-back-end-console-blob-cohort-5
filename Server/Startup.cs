14cba2d4c25e560957e58c605b5db958cc57a992
craig2812 <craig2812@gmail.com>
2021-04-01 15:45:15 +0100

    cors working, but default policy only

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,65 +7,92 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace Server
 {
     public class Startup
     {
-        readonly string frontEnd = "_frontEnd";
+        readonly string MyAllowSpecificOrigins = "_MyAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
-
         {
             Configuration = configuration;
         }
-
+        
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-      {
-          options.AddPolicy(name: frontEnd,
-                            builder =>
-                            {
-                                builder.WithOrigins("http://localhost:3000")
-                                .AllowAnyHeader()
-                                .AllowAnyMethod()
-                                .AllowCredentials();
-                            });
-      
-
-        services.AddTransient<IRepository<CountryObj>, CountryRepository>();
+            // services.AddTransient<>();
+                        
+            services.AddTransient<IRepository<CountryObj>, CountryRepository>();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddCors(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Server", Version = "v1" });
-            });
-        });
 
-    }
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Server v1"));
+//Default Policy with Specified Server Host 
+                // options.AddDefaultPolicy(
+                //                   builder =>
+                //                   {
+                //                       // Add cross origin allowed domain
+                //                       builder.WithOrigins("http://localhost:3000")
+                //                                     .AllowAnyHeader()
+                //                                     .AllowAnyMethod();
+                //                   });
+
+//Made Policy with Origin Specified 
+                // options.AddPolicy(name: MyAllowSpecificOrigins,
+                //     builder => {
+                //         builder.WithOrigins("http://localhost:3000/).AllowAnyHeader().AllowAnyMethod();
+                //     }  
+
+//Default Policy 
+                       options.AddDefaultPolicy(
+                    builder => {
+                        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                    }             
+                                    );
+            });
+            services.AddControllers();
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "src", Version = "v1" });
+            // });
         }
 
-        app.UseHttpsRedirection();
-
-        app.UseRouting();
-        app.UseCors(frontEnd);
-        app.UseAuthorization();
-
-        app.UseEndpoints(endpoints =>
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            endpoints.MapControllers();
-        });
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "src v1"));
+            // }
+
+            app.UseRouting();
+
+//Made Policy 
+            // app.UseCors(MyAllowSpecificOrigins);
+
+//Dafault Policy 
+            app.UseCors();            
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
 }
-}
+
+
+
